f250e3c8eb3106454566e92cff2d6b533cba7313
NickTikhomirov <nicktikhomirov02@gmail.com>
2022-07-17 16:32:40 +0300

    Роутинг титульников, постбилд

--- scripts/tex-postbuild.js
+++ scripts/tex-postbuild.js
@@ -5,31 +5,50 @@ const {
     getAbbreviation,
 } = require('@md-to-latex/manager/dist/utils/filename');
 
-function __postbuild() {
-    const config = readConfig('md-to-latex-title.yml');
 
-    const groupString = translit(config.title.report.author.group);
+function __format_name_array(inArray, defaultName){
+    if (inArray.length === 0)
+        return defaultName;
+    return inArray.join('_');
+}
+
+function __get_filename(config){
     const authorString = translit(config.general.author.name).replace(
         /[ .]/g,
         '',
     );
-    const subjectAbbreviation = getAbbreviation(
-        config.title.report.document.subject,
-    );
-    const typeAbbreviation = getAbbreviation(
-        config.title.report.document.typeDative,
-    );
+    const year = ""+config.general.document.year;
+    const topic = config.general.document.topic;
 
-    const filename =
-        authorString +
-        '_' +
-        groupString +
-        '_' +
-        subjectAbbreviation +
-        '_' +
-        typeAbbreviation +
-        '.pdf';
+    if (config.title === undefined){
+        return __format_name_array([authorString, topic, year]);
+    }
 
+    if(config.title.practice !== undefined){
+        const practice = 'Practice';
+        const course = "C"+config.title.practice.author.afterCourse;
+
+        return __format_name_array([authorString, practice, course, year]);
+    }
+
+    if(config.title.report !== undefined){
+        const groupString = translit(config.title.report.author.group);
+        const subjectAbbreviation = getAbbreviation(
+            config.title.report.document.subject,
+        );
+        const typeAbbreviation = getAbbreviation(
+            config.title.report.document.typeDative,
+        );
+
+        return __format_name_array([authorString, groupString, subjectAbbreviation, typeAbbreviation]);
+    }
+
+    return "undefined"; // this code is expected to be unreachable
+}
+
+function __postbuild() {
+    const config = readConfig('md-to-latex-title.yml');
+    const filename = __get_filename(config) + '.pdf';
     fs.copyFileSync('out/index.pdf', filename);
 }
 
